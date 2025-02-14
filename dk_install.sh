a3ea8e752519c5c4a606bba0de5f18f748cd1f59
developer <developer@rta-vrte-sk.virtual.etas.com>
2025-02-14 09:48:36 +0000

    support to install artiface for dk_ara_demo

--- dk_install.sh
+++ dk_install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "------------------------------------------------------------------------------------------------------------------------------------"
+echo "Start dreamOS installation !!!!!!!!!!!!!"
+echo "------------------------------------------------------------------------------------------------------------------------------------"
 # Determine the user who ran the command
 if [ -n "$SUDO_USER" ]; then
     # Command was run with sudo
@@ -64,6 +67,8 @@ cd /home/$DK_USER/.dk
 echo "Create dk_network ..."
 docker network create dk_network
 
+echo "------------------------------------------------------------------------------------------------------------------------------------"
+echo "------------------------------------------------------------------------------------------------------------------------------------"
 echo "Install installation repo"
 DK_INSTALLATION_DIR="$HOME_DIR/.dk/dk_installation"
 if [ ! -d "$DK_INSTALLATION_DIR" ]; then
@@ -74,6 +79,8 @@ else
     echo "Folder $DK_INSTALLATION_DIR already exists."
 fi
 
+echo "------------------------------------------------------------------------------------------------------------------------------------"
+echo "------------------------------------------------------------------------------------------------------------------------------------"
 echo "Install APP Python SDK"
 APP_PY_SDK_DIR="$HOME_DIR/.dk/dk_app_python_template"
 if [ ! -d "$APP_PY_SDK_DIR" ]; then
@@ -84,40 +91,72 @@ else
     echo "Folder $APP_PY_SDK_DIR already exists."
 fi
 
-echo "Install APP CPP SDK"
-APP_CPP_SDK_DIR="$HOME_DIR/.dk/dk_app_cpp_template"
-if [ ! -d "$APP_CPP_SDK_DIR" ]; then
-    # Folder does not exist, do something
-    echo "Folder $APP_CPP_SDK_DIR does not exist. Downloading ..."
-    git clone https://github.com/ppa2hc/dk_app_cpp_template.git
-else
-    echo "Folder $APP_CPP_SDK_DIR already exists."
-fi
-
+echo "------------------------------------------------------------------------------------------------------------------------------------"
+echo "------------------------------------------------------------------------------------------------------------------------------------"
 echo "Remove Only Dangling Images (No Tags)"
 docker image prune -f
 
+echo "------------------------------------------------------------------------------------------------------------------------------------"
+echo "------------------------------------------------------------------------------------------------------------------------------------"
 echo "Install base image for velocitas py app ..."
 docker pull $DOCKER_HUB_NAMESPACE/dk_app_python_template:baseimage
 
-echo "Install base image for adaptive AR cpp app ..."
-docker pull $DOCKER_HUB_NAMESPACE/dk_app_cpp_template:latest
-
+echo "------------------------------------------------------------------------------------------------------------------------------------"
+echo "------------------------------------------------------------------------------------------------------------------------------------"
 echo "Install dk_manager ..."
 docker pull $DOCKER_HUB_NAMESPACE/dk_manager:latest
 docker stop dk_manager; docker rm dk_manager; docker run -d -it --name dk_manager $LOG_LIMIT_PARAM $DOCKER_SHARE_PARAM  -v $HOME_DIR/.dk:/app/.dk --restart unless-stopped -e USER=$DK_USER -e DOCKER_HUB_NAMESPACE=$DOCKER_HUB_NAMESPACE -e ARCH=$ARCH $DOCKER_HUB_NAMESPACE/dk_manager:latest
 
+echo "------------------------------------------------------------------------------------------------------------------------------------"
+echo "------------------------------------------------------------------------------------------------------------------------------------"
 echo "Install vss_generation ..."
 docker pull $DOCKER_HUB_NAMESPACE/dk_vssgeneration_image:vss4.0
 docker rm vssgen;docker run -it --name vssgen -v $HOME_DIR/.dk/dk_vssgeneration/:/app/dk_vssgeneration -v $HOME_DIR/.dk/dk_manager/vssmapping/vssmapping_overlay.vspec:/app/.dk/dk_manager/vssmapping/vssmapping_overlay.vspec:ro $LOG_LIMIT_PARAM $DOCKER_HUB_NAMESPACE/dk_vssgeneration_image:vss4.0
 
+echo "------------------------------------------------------------------------------------------------------------------------------------"
+echo "------------------------------------------------------------------------------------------------------------------------------------"
 echo "Install vehicle data broker ... "
 docker pull ghcr.io/eclipse-kuksa/kuksa-databroker:0.4.4
 docker stop vehicledatabroker ; docker rm vehicledatabroker ; docker run -d -it --name vehicledatabroker -e KUKSA_DATA_BROKER_METADATA_FILE=/app/.dk/dk_vssgeneration/vss.json -e KUKSA_DATA_BROKER_PORT=55555 -e 50001 -e 3500 -v $HOME_DIR/.dk/dk_vssgeneration/vss.json:/app/.dk/dk_vssgeneration/vss.json --restart unless-stopped --network dk_network -p 55555:55555 $LOG_LIMIT_PARAM ghcr.io/eclipse-kuksa/kuksa-databroker:0.4.4 --insecure --vss /app/.dk/dk_vssgeneration/vss.json
 
+echo "------------------------------------------------------------------------------------------------------------------------------------"
+echo "------------------------------------------------------------------------------------------------------------------------------------"
+# Install dk_ara_demo
+dk_ara_demo=""
+# Loop through all input arguments
+for arg in "$@"; do
+    # Check if the argument starts with dk_ara_demo=
+    if [[ "$arg" == dk_ara_demo=* ]]; then
+        # Extract the value after the equal sign
+        dk_ara_demo="${arg#*=}"
+    fi
+done
+if [[ "$dk_ara_demo" == "true" ]]; then
+    echo "Install APP CPP SDK"
+    APP_CPP_SDK_DIR="$HOME_DIR/.dk/dk_app_cpp_template"
+    if [ ! -d "$APP_CPP_SDK_DIR" ]; then
+        # Folder does not exist, do something
+        echo "Folder $APP_CPP_SDK_DIR does not exist. Downloading ..."
+        git clone https://github.com/ppa2hc/dk_app_cpp_template.git
+    else
+        echo "Folder $APP_CPP_SDK_DIR already exists."
+    fi
+
+    echo "Install base image for adaptive AR cpp app ..."
+    docker pull $DOCKER_HUB_NAMESPACE/dk_app_cpp_template:latest
+
+    echo "Install kuksa.val for vss ara::provider demo"
+    docker pull ghcr.io/eclipse/kuksa.val/kuksa-val:0.2.5
+    docker stop kuksa_val_server ; docker rm kuksa_val_server ; docker run -d --name kuksa_val_server --restart unless-stopped -it -p 50051:50051 -p 8090:8090 $LOG_LIMIT_PARAM -e LOG_LEVEL=ALL -e KUKSAVAL_OPTARGS="--insecure" ghcr.io/eclipse/kuksa.val/kuksa-val:0.2.5
+fi
+
+echo "------------------------------------------------------------------------------------------------------------------------------------"
+echo "------------------------------------------------------------------------------------------------------------------------------------"
 echo "Install App/service installation service ... "
 docker pull $DOCKER_HUB_NAMESPACE/dk_appinstallservice:latest
 
+echo "------------------------------------------------------------------------------------------------------------------------------------"
+echo "------------------------------------------------------------------------------------------------------------------------------------"
 # Install dk_ivi
 dk_ivi_value=""
 # Loop through all input arguments
@@ -128,7 +167,6 @@ for arg in "$@"; do
         dk_ivi_value="${arg#*=}"
     fi
 done
-
 if [[ "$dk_ivi_value" == "true" ]]; then
     echo "enable xhost local"
 	$CURRENT_DIR/dk_enable_xhost.sh
@@ -147,6 +185,9 @@ else
     echo "To Install dk_ivi, run './dk_install dk_ivi=true'"
 fi
 
-echo "------------------------------------------------------------------"
+echo "------------------------------------------------------------------------------------------------------------------------------------"
+echo "------------------------------------------------------------------------------------------------------------------------------------"
+
+echo "------------------------------------------------------------------------------------------------------------------------------------"
 echo "Please reboot your system for dreamSW to take effect !!!!!!!!!!!!!"
-echo "------------------------------------------------------------------"
+echo "------------------------------------------------------------------------------------------------------------------------------------"
