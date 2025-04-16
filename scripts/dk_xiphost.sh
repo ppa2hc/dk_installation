007c99f0bc8aa002eaf83a1dff69d489da4ed15c
APACrst1hc <Sam.TranNhu@vn.bosch.com>
2025-04-16 09:48:36 +0100

    skip update xiphost in case of patchupdate from dev env.

--- scripts/dk_xiphost.sh
+++ scripts/dk_xiphost.sh
@@ -16,6 +16,35 @@ echo "dk_ara_demo: $dk_ara_demo"
 echo "dk_ivi_value: $dk_ivi_value"
 
 while true; do
+    # Wait before the next check (e.g., 10 seconds)
+    sleep 5
+
+    ##########################################################################################
+    ###############  Vehicle.SwUpdate.XipHost.PatchUpdateTrigger ##################################
+    ##########################################################################################
+    # Use command substitution to capture the output of the child script.
+    result=$(/home/.dk/dk_swupdate/dk_kuksa_client.sh getValue Vehicle.SwUpdate.PatchUpdateTrigger)
+    # Capture the exit code from the child script.
+    exit_code=$?
+    # Print the output and the exit code.
+    echo "PatchUpdateTrigger: The child script returned: '$result'"
+    # Execute SW update if true
+    if [ $exit_code -eq 0 ]; then
+        echo "PatchUpdateTrigger: Child script succeeded."
+        if [[ $result == *'true'* ]]; then
+            echo "PatchUpdateTrigger is true, executing update command..."
+            # Execute your update command here:
+            /home/.dk/dk_swupdate/dk_kuksa_client.sh setValue Vehicle.SwUpdate.XipHost.UpdateTrigger False
+            /home/.dk/dk_swupdate/dk_kuksa_client.sh setValue Vehicle.SwUpdate.XipHost.PercentageDone 100
+            echo "PatchUpdateTrigger: It is not allowed to update host in PatchUpdateTrigger.... So skip this xiphost update."
+            continue
+        else
+            echo "PatchUpdateTrigger is false. Checking again..."
+        fi
+    else
+        echo "PatchUpdateTrigger: Child script failed with exit code: $exit_code"
+    fi
+
     ##########################################################################################
     ###############  Vehicle.SwUpdate.XipHost.UpdateTrigger ##################################
     ##########################################################################################
@@ -24,12 +53,12 @@ while true; do
     # Capture the exit code from the child script.
     exit_code=$?
     # Print the output and the exit code.
-    echo "The child script returned: '$result'"
+    echo "UpdateTrigger: The child script returned: '$result'"
     # Execute SW update if true
     if [ $exit_code -eq 0 ]; then
         echo "Child script succeeded."
         if [[ $result == *'true'* ]]; then
-            echo "UpdateTrigger is true, executing update command..."
+            echo "UpdateTrigger: UpdateTrigger is true, executing update command..."
             # Execute your update command here:
             /home/.dk/dk_swupdate/dk_kuksa_client.sh setValue Vehicle.SwUpdate.XipHost.UpdateTrigger False
             ret=$("$HOME_DIR/.dk/dk_swupdate/dk_installation/swpackage/xip/xiphost.sh")
@@ -40,45 +69,11 @@ while true; do
                 /home/.dk/dk_swupdate/dk_kuksa_client.sh setValue Vehicle.SwUpdate.Status 7
             fi
         else
-            echo "UpdateTrigger is false. Checking again..."
+            echo "UpdateTrigger: UpdateTrigger is false. Checking again..."
         fi
     else
-        echo "Child script failed with exit code: $exit_code"
+        echo "UpdateTrigger: Child script failed with exit code: $exit_code"
     fi
-
-
-    ##########################################################################################
-    ###############  Vehicle.SwUpdate.XipHost.PatchUpdateTrigger ##################################
-    ##########################################################################################
-    # Use command substitution to capture the output of the child script.
-    result=$(/home/.dk/dk_swupdate/dk_kuksa_client.sh getValue Vehicle.SwUpdate.XipHost.PatchUpdateTrigger)
-    # Capture the exit code from the child script.
-    exit_code=$?
-    # Print the output and the exit code.
-    echo "The child script returned: '$result'"
-    # Execute SW update if true
-    if [ $exit_code -eq 0 ]; then
-        echo "Child script succeeded."
-        if [[ $result == *'true'* ]]; then
-            echo "PatchUpdateTrigger is true, executing update command..."
-            # Execute your update command here:
-            /home/.dk/dk_swupdate/dk_kuksa_client.sh setValue Vehicle.SwUpdate.XipHost.PatchUpdateTrigger False
-            ret=$("$HOME_DIR/.dk/dk_swupdate/dk_patch/swpackage/xip/xiphost.sh")
-            e_code=$?
-            if [ $e_code -eq 0 ]; then
-                /home/.dk/dk_swupdate/dk_kuksa_client.sh setValue Vehicle.SwUpdate.XipHost.PercentageDone 100
-            else
-                /home/.dk/dk_swupdate/dk_kuksa_client.sh setValue Vehicle.SwUpdate.Status 7
-            fi
-        else
-            echo "PatchUpdateTrigger is false. Checking again..."
-        fi
-    else
-        echo "Child script failed with exit code: $exit_code"
-    fi
-
-    # Wait before the next check (e.g., 10 seconds)
-    sleep 5
 done
 
 echo "Exiting update check loop."
