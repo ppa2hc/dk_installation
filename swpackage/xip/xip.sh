7f06396c7fa9e85bc804c385b8a27136cb905245
APACrst1hc <Sam.TranNhu@vn.bosch.com>
2025-04-16 09:31:28 +0100

    trim down xip scripts

--- swpackage/xip/xip.sh
+++ swpackage/xip/xip.sh
@@ -54,6 +54,25 @@ docker pull ghcr.io/eclipse/kuksa.val/kuksa-client:0.4.2
 
 echo "------------------------------------------------------------------------------------------------------------------------------------"
 echo "------------------------------------------------------------------------------------------------------------------------------------"
+# Install dk_ivi
+if [[ "$dk_ivi_value" == "true" ]]; then
+    echo "Instal dk_ivi ..."
+    docker pull $DOCKER_HUB_NAMESPACE/dk_ivi:latest
+
+    echo "Checking for NVIDIA Target Board..."
+    if [ -f "/etc/nv_tegra_release" ]; then
+        echo "NVIDIA Jetson board detected."
+        docker stop dk_ivi; docker rm dk_ivi ; docker run -d -it --name dk_ivi -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=:0 -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR -e QT_QUICK_BACKEND=software --restart unless-stopped $LOG_LIMIT_PARAM $DOCKER_SHARE_PARAM -v $HOME_DIR/.dk:/app/.dk -e DKCODE=dreamKIT -e DK_USER=$DK_USER -e DK_DOCKER_HUB_NAMESPACE=$DOCKER_HUB_NAMESPACE -e DK_ARCH=$ARCH -e DK_CONTAINER_ROOT="/app/.dk/" $DOCKER_HUB_NAMESPACE/dk_ivi:latest
+    else
+        echo "Not NVIDIA board."
+        docker stop dk_ivi; docker rm dk_ivi ; docker run -d -it --name dk_ivi -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=:0 -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR --device /dev/dri:/dev/dri --restart unless-stopped $LOG_LIMIT_PARAM $DOCKER_SHARE_PARAM -v $HOME_DIR/.dk:/app/.dk -e DKCODE=dreamKIT -e DK_USER=$DK_USER -e DK_DOCKER_HUB_NAMESPACE=$DOCKER_HUB_NAMESPACE -e DK_ARCH=$ARCH -e DK_CONTAINER_ROOT="/app/.dk/" $DOCKER_HUB_NAMESPACE/dk_ivi:latest
+    fi
+else
+    echo "To Install dk_ivi, run './dk_install dk_ivi=true'"
+fi
+
+echo "------------------------------------------------------------------------------------------------------------------------------------"
+echo "------------------------------------------------------------------------------------------------------------------------------------"
 echo "Remove Only Dangling Images (No Tags)"
 docker image prune -f
 
