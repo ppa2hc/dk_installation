5f70b1aef2a8563a8e7487eae13c95f6ce6ccc3c
APACrst1hc <Sam.TranNhu@vn.bosch.com>
2025-04-17 16:44:09 +0100

    dk_ivi shoud be installed at last

--- dk_install.sh
+++ dk_install.sh
@@ -215,37 +215,6 @@ docker pull $DOCKER_HUB_NAMESPACE/dk_appinstallservice:latest
 
 echo "------------------------------------------------------------------------------------------------------------------------------------"
 echo "------------------------------------------------------------------------------------------------------------------------------------"
-# Install dk_ivi
-dk_ivi_value=""
-# Loop through all input arguments
-for arg in "$@"; do
-    # Check if the argument starts with dk_ivi=
-    if [[ "$arg" == dk_ivi=* ]]; then
-        # Extract the value after the equal sign
-        dk_ivi_value="${arg#*=}"
-    fi
-done
-if [[ "$dk_ivi_value" == "true" ]]; then
-    echo "enable xhost local"
-	$CURRENT_DIR/scripts/dk_enable_xhost.sh
-    echo "Instal dk_ivi ..."
-    docker pull $DOCKER_HUB_NAMESPACE/dk_ivi:latest
-
-    echo "Checking for NVIDIA Target Board..."
-    if [ -f "/etc/nv_tegra_release" ]; then
-        echo "NVIDIA Jetson board detected."
-        docker stop dk_ivi; docker rm dk_ivi ; docker run -d -it --name dk_ivi -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=:0 -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR -e QT_QUICK_BACKEND=software --restart unless-stopped --network host $LOG_LIMIT_PARAM $DOCKER_SHARE_PARAM -v $HOME_DIR/.dk:/app/.dk -e DKCODE=dreamKIT -e DK_USER=$DK_USER -e DK_DOCKER_HUB_NAMESPACE=$DOCKER_HUB_NAMESPACE -e DK_ARCH=$ARCH -e DK_CONTAINER_ROOT="/app/.dk/" $DOCKER_HUB_NAMESPACE/dk_ivi:latest
-    else
-        echo "Not NVIDIA board."
-        docker stop dk_ivi; docker rm dk_ivi ; docker run -d -it --name dk_ivi -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=:0 -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR --device /dev/dri:/dev/dri --restart unless-stopped --network host $LOG_LIMIT_PARAM $DOCKER_SHARE_PARAM -v $HOME_DIR/.dk:/app/.dk -e DKCODE=dreamKIT -e DK_USER=$DK_USER -e DK_DOCKER_HUB_NAMESPACE=$DOCKER_HUB_NAMESPACE -e DK_ARCH=$ARCH -e DK_CONTAINER_ROOT="/app/.dk/" $DOCKER_HUB_NAMESPACE/dk_ivi:latest
-    fi
-else
-    echo "To Install dk_ivi, run './dk_install dk_ivi=true'"
-fi
-
-
-echo "------------------------------------------------------------------------------------------------------------------------------------"
-echo "------------------------------------------------------------------------------------------------------------------------------------"
 echo "Store environment variables"
 
 # Define the output file (you can change the path as needed)
@@ -326,6 +295,36 @@ echo "--------------------------------------------------------------------------
 echo "Install OS SW Update service ... "
 docker pull $DOCKER_HUB_NAMESPACE/dk_swupdate:latest
 docker stop dk_swupdate; docker rm dk_swupdate; docker run -d -it --name dk_swupdate $LOG_LIMIT_PARAM -v /var/run/docker.sock:/var/run/docker.sock --network host -v $HOME_DIR/.dk:/app/.dk --restart unless-stopped -e DK_HOME="/app" $DOCKER_HUB_NAMESPACE/dk_swupdate:latest
+
+echo "------------------------------------------------------------------------------------------------------------------------------------"
+echo "------------------------------------------------------------------------------------------------------------------------------------"
+# Install dk_ivi
+dk_ivi_value=""
+# Loop through all input arguments
+for arg in "$@"; do
+    # Check if the argument starts with dk_ivi=
+    if [[ "$arg" == dk_ivi=* ]]; then
+        # Extract the value after the equal sign
+        dk_ivi_value="${arg#*=}"
+    fi
+done
+if [[ "$dk_ivi_value" == "true" ]]; then
+    echo "enable xhost local"
+	$CURRENT_DIR/scripts/dk_enable_xhost.sh
+    echo "Instal dk_ivi ..."
+    docker pull $DOCKER_HUB_NAMESPACE/dk_ivi:latest
+
+    echo "Checking for NVIDIA Target Board..."
+    if [ -f "/etc/nv_tegra_release" ]; then
+        echo "NVIDIA Jetson board detected."
+        docker stop dk_ivi; docker rm dk_ivi ; docker run -d -it --name dk_ivi -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=:0 -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR -e QT_QUICK_BACKEND=software --restart unless-stopped --network host $LOG_LIMIT_PARAM $DOCKER_SHARE_PARAM -v $HOME_DIR/.dk:/app/.dk -e DKCODE=dreamKIT -e DK_USER=$DK_USER -e DK_DOCKER_HUB_NAMESPACE=$DOCKER_HUB_NAMESPACE -e DK_ARCH=$ARCH -e DK_CONTAINER_ROOT="/app/.dk/" $DOCKER_HUB_NAMESPACE/dk_ivi:latest
+    else
+        echo "Not NVIDIA board."
+        docker stop dk_ivi; docker rm dk_ivi ; docker run -d -it --name dk_ivi -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=:0 -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR --device /dev/dri:/dev/dri --restart unless-stopped --network host $LOG_LIMIT_PARAM $DOCKER_SHARE_PARAM -v $HOME_DIR/.dk:/app/.dk -e DKCODE=dreamKIT -e DK_USER=$DK_USER -e DK_DOCKER_HUB_NAMESPACE=$DOCKER_HUB_NAMESPACE -e DK_ARCH=$ARCH -e DK_CONTAINER_ROOT="/app/.dk/" $DOCKER_HUB_NAMESPACE/dk_ivi:latest
+    fi
+else
+    echo "To Install dk_ivi, run './dk_install dk_ivi=true'"
+fi
 
 echo "------------------------------------------------------------------------------------------------------------------------------------"
 echo "------------------------------------------------------------------------------------------------------------------------------------"
